c32d9590d6ece2372b7e7252cc07600d8ee2ed55
ShlenkinVV <mr.shlenkin@mail.ru>
2024-12-18 18:47:08 +0400

    initial commit

---
+++ module_13_3.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN=os.getenv('TOKEN')
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def all_massages(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
